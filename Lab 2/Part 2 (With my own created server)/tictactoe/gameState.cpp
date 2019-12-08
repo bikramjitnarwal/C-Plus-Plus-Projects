cb4d1433090b7f25f835d03caa129535f071a0da
Bikramjit Narwal <bikramjitnarwal@hotmail.com>
2019-12-07 23:18:45 -0500

    Update gameState.cpp

--- Lab 2/Part 2 (With my own created server)/tictactoe/gameState.cpp
+++ Lab 2/Part 2 (With my own created server)/tictactoe/gameState.cpp
@@ -1,10 +1,3 @@
-//
-//  gameState.cpp
-//  TicTacTo
-//
-//  Created by Tarek Abdelrahman on 2019-06-07.
-//  Copyright © 2019 Tarek Abdelrahman. All rights reserved.
-
 #include "tictactoe.h"
 #include "gameState.h"
 
